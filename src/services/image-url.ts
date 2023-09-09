25401f4c3bde1bb0ba84db811125079a8bd3c435
vytautasdev <vytautasdev@gmail.com>
2023-09-09 21:19:30 +0100

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import noImagePlaceholder from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return noImagePlaceholder;
 
   const target = "media/";
   const index = url.indexOf(target) + target.length;
